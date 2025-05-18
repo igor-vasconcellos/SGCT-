08a6410e4602e5993a1b70a0f8af213648092092
Igor Vasconcellos <143509020+igor-vasconcellos@users.noreply.github.com>
2025-05-18 11:47:15 -0300

    Delete sgct_sistema/SGCT--main directory

--- sgct_sistema/SGCT--main/public/js/utils.js
+++
@@ -1,7 +0,0 @@
-// Função para validar o formato de um e-mail
-function cadastrarUsuario(nome, email, senha, tipo) {
-  const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
-  const id = Date.now();
-  usuarios.push({ id, nome, email, senha, tipo });
-  localStorage.setItem('usuarios', JSON.stringify(usuarios));
-}
